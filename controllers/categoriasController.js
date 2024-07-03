eafc41ab0e5a440276424f2d0c53f5a9414e6646
Lucas Lopez <lucas-glr@hotmail.com>
2024-07-03 20:12:10 -0300

    controllers categoria , producto ...

---
+++ controllers/categoriasController.js
@@ -0,0 +1,54 @@
+const modeloCategoria = require("../Model/categoriasModel.js")
+
+/* CRUD - CREATE - READ - UPDATE - DELETE */
+
+/* FUNCION PARA TRAER TODOS LOS POSTEOS = READ - GET */
+
+const obtenerCategorias =async(req,res)=>{
+    try {
+        const categorias = await modeloCategoria.findAll()
+        res.json(categorias);
+    }
+     catch (error) {
+        res.json({message:error.message})
+    }
+}
+
+const obtenerCategoria=async(res,req)=>{
+    try {
+        const categoria = await modeloCategoria.findByPk(req.params.id);
+        res.json(categoria)
+    } catch (error) {
+        res.json({message:error.message})
+    }
+}
+
+const crearCategoria = async(req,res)=>{
+    try {
+        const newCategoria = await modeloCategoria.create(req.body)
+        res.json("registro creado correctamente")
+    } catch (error) {
+        res.json({message:error.message})
+    }
+}
+
+const editarCategoria=async(res,req)=>{
+    try {
+        await modeloCategoria.update(req.body,
+            {where:{id:req.params.id}})
+            res.json("categoria editada correcamente")
+    } catch (error) {
+        res.json({message:error.message})
+    }
+}
+
+const borrarCategoria = async (req,res)=>{
+    try {
+        await modeloCategoria.destroy( { where:{id:req.params.id}})
+        res.json("categoria borrado correctamente")
+    } catch (error) {
+        res.json({message:error.message})
+    }
+}
+
+module.exports ={obtenerCategorias,crearCategoria,obtenerCategoria,editarCategoria,borrarCategoria}
---
+++ controllers/nosotrosController.js
@@ -0,0 +1,8 @@
+const obtenerTexto=()=>{
+    console.log(`TEXTO GENERADO`);
+}
+
+const obtenerId=()=>{
+    console.log(`ID`);
+}
+module.exports= {obtenerTexto,obtenerId};
