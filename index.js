4a94c0c9c3f7bafa2315a4577796ba07cd850f27
Lucas Lopez <lucas-glr@hotmail.com>
2024-07-03 20:17:54 -0300

    index.js con rutas  ...

--- index.js
+++ index.js
@@ -1,44 +1,35 @@
-const  db = require ("./data/db.js");
-
-const express = require ("express");
-
+const express = require("express");
+const cors = require("cors");
+const db = require("./data/db.js")
+const usuarioControlador = require("./routes/usuariosRouter.js");
+const categoriasController = require("./routes/categoriasRouter.js");
+const productoController = require("./routes/productosRouter.js");
 const app = express();
-
-const cors = require ("cors"); //Intercambio de recursos de origen cruzado
-
 const port = 3030;
 
-//Modulo propio para el ruteo
-const PostRouter = require ("./routes/PostRouter.js");
-
-app.use(cors()); //habilito el intercambio de información
-app.use(express.json()) //analiza los request
-
-//Modularizando mediante el use
-
-app.use("/posteos", PostRouter)
-
-app.get("/",(request, response)=>{
-    response.send("Bienvenido al Home");
+app.get("/",(req,res)=>{
+  res.send(`PAGINA DE INICIO`);
 })
 
-
-app.get("/conocenos",(request, response)=>{
-    response.send("Sección conocenos");
-})
-
-//Función asíncrona que comprueba la conexión
-const conexionDB = async ()=>{
-    try {
-        await db.authenticate();
-        console.log("Conectado a la base de datos");        
-    } catch (error) {
-        console.log(`El error es : ${error}`)
-    }
-}
+app.use(cors()) // habilito el intercambio de información
+app.use(express.json()) // analiza los request
+app.use("/usuario",usuarioControlador);
+app.use("/categoria",categoriasController);
+app.use("/producto",productoController);
 
 
 app.listen(port,()=>{
-    conexionDB(); //llamo a la función asíncrona
-    console.log(`Server corriendo en el puerto ${port}`)
+      conexionDB()
+      console.log(`Servidor Ok en el puerto ${port}`);
 })
+
+// conexion a la base de datos
+
+const conexionDB = async ()=>{
+  try {
+      await db.authenticate()
+      console.log(`Conectado Ok a la Base de datos`);
+  } catch (error) {
+      console.log(`Hay un error y es el siguiente : ${error}`);
+  }
+}
