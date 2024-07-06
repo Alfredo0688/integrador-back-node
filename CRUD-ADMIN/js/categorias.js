5fed0af916e6d5c20e869d2afa340a7fb1b6fa60
Lucas Lopez <lucas-glr@hotmail.com>
2024-07-06 12:31:11 -0300

    CRUD CATEGORIA , PRODUCTO

---
+++ CRUD-ADMIN/js/categorias.js
@@ -0,0 +1,82 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+  const bodyTablaCategoria = document.querySelector("#bodyTablaCategoria");
+  const formulario = document.querySelector("#formCategoria");
+
+  const fetchCategoria = async () => {
+    try {
+      const respuesta = await axios.get(`http://localhost:3030/categorias/`);
+      const categorias = respuesta.data;
+
+      bodyTablaCategoria.innerHTML = "";
+      categorias.forEach((categoria) => {
+        // creo una nueva fila
+        const fila = document.createElement("tr");
+        //Crear celdas para titulo, contenido y acciones
+        const celdaNombre = document.createElement("td");
+        const celdaAcciones = document.createElement("td");
+
+        // asignar el contenido de las celdas
+        celdaNombre.textContent = categoria.nombre;
+
+        // crear el boton para editar un posteo
+        const botonEditar = document.createElement("button");
+        botonEditar.classList.add("btn");
+        botonEditar.classList.add("btn-warning");
+        botonEditar.classList.add("ml-1");
+        botonEditar.textContent = "Editar";
+        botonEditar.addEventListener("click", () => {
+          // Redirigir a la pagina de edicion  con el ID del post en la url
+          //window.location.href = `edit.html?id=${posteo.id}`;
+        });
+
+        // crear el boton de eliminar
+        const botonEliminar = document.createElement("button");
+        botonEliminar.classList.add("btn");
+        botonEliminar.classList.add("btn-danger");
+        botonEliminar.classList.add("mr-2");
+        botonEliminar.textContent = "Eliminar";
+
+        botonEliminar.addEventListener("click", () => {
+          borraCategoria(categoria.id);
+        });
+
+        // agregar los botones a la celda de acciones
+        celdaAcciones.appendChild(botonEliminar);
+        celdaAcciones.appendChild(botonEditar);
+
+        // agregamos las celdas a la fila
+        fila.appendChild(celdaNombre);
+        fila.appendChild(celdaAcciones);
+
+        // Agregar la fila al cuerpo de la tabla
+        bodyTablaCategoria.appendChild(fila);
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  fetchCategoria();
+
+  //agregar categoria
+  formulario.addEventListener("submit",()=>{
+    try {
+        alert("hola");
+    } catch (error) {
+        
+    }
+  })
+
+
+  // funcion para eliminar unposteo
+  const borraCategoria = async (id) => {
+    try {
+      await axios.delete(`http://localhost:3030/categoria/${id}`);
+      // recargare la lista de posteos despues de elminar
+      fetchCategoria();
+    } catch (error) {
+      console.error(`Error al eliminar el post : ${error}`);
+    }
+  };
+});
--- CRUD-ADMIN/js/productos.js
+++ CRUD-ADMIN/js/productos.js
@@ -3,12 +3,34 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const bodyTablaProducto = document.querySelector("#tablaProducto");
 
+  const selectCategoria = document.querySelector("#slc_categoria");
+
+  //cargar select
+  const fetchCategoria = async () => {
+    try {
+      const respuesta = await axios.get(`http://localhost:3030/categorias/`);
+      const categorias = respuesta.data;
+      selectCategoria.innerHTML = "";
+      selectCategoria.innerHTML=(`<option> SELECCIONAR </option>`) 
+      categorias.forEach((categoria) => {
+        const optional = document.createElement(`option`);
+        optional.textContent = categoria.nombre;
+        optional.value=categoria.id;
+        selectCategoria.appendChild(optional);
+      });
+      
+    } catch (error) {
+      console.error(`Error al postear: ${error}`);
+    }
+  };
+
   const fetchProductos = async () => {
     try {
       const respuesta = await axios.get(`http://localhost:3030/productos/`);
       const productos = respuesta.data;
-      tablaProducto.innerHTML = "";
-      productos.forEach(producto => {
+      
+      bodyTablaProducto.innerHTML = "";
+      productos.forEach((producto) => {
         // creo una nueva fila
         const fila = document.createElement("tr");
         //Crear celdas para titulo, contenido y acciones
@@ -17,17 +39,24 @@ document.addEventListener("DOMContentLoaded", () => {
         const celdaModelo = document.createElement("td");
         const celdaCantidad = document.createElement("td");
         const celdaUrlImagen = document.createElement("td");
+        const celdaAcciones= document.createElement("td")
 
         // asignar el contenido de las celdas
         celdaNombre.textContent = producto.nombre;
         celdaMarca.textContent = producto.marca;
         celdaModelo.textContent = producto.modelo;
         celdaCantidad.textContent = producto.cantidad;
-        celdaUrlImagen.textContent = producto.foto;
+
+        //crear etiqueta img
+        const imagen=document.createElement("img");
+        imagen.src=producto.foto
+        imagen.classList.add('img-thumbnail')
+        celdaUrlImagen.textContent = imagen;
 
         // crear el boton para editar un posteo
         const botonEditar = document.createElement("button");
         botonEditar.textContent = "Editar";
+        botonEditar.classList.add('btn btn-warning')
         botonEditar.addEventListener("click", () => {
           // Redirigir a la pagina de edicion  con el ID del post en la url
           //window.location.href = `edit.html?id=${posteo.id}`;
@@ -36,6 +65,7 @@ document.addEventListener("DOMContentLoaded", () => {
         // crear el boton de eliminar
         const botonEliminar = document.createElement("button");
         botonEliminar.textContent = "Eliminar";
+        botonEliminar.classList.add('btn btn-danger')
         botonEliminar.addEventListener("click", () => {
           //borrarProducto(producto.id);
         });
@@ -55,30 +85,33 @@ document.addEventListener("DOMContentLoaded", () => {
         bodyTablaProducto.appendChild(fila);
       });
     } catch (error) {
-      console.error (`Error al obtener los post : ${error}`)
+      //console.error(`Error al obtener los post : ${error}`);
     }
   };
+  
   // Funcion para crear un nuevo Producto
-  formulario.addEventListener("submit",async function(event){
+  formulario.addEventListener("submit", async function (event) {
     event.preventDefault();
     const nuevoProducto = {
-        nombre:document.querySelector("#nombre").value,
-        marca : document.querySelector("#marca").value,
-        modelo:document.querySelector("#modelo").value,
-        precio:document.querySelector("#precio").value,
-        foto:document.querySelector("#imagen").value,
-        id_categoria:1
+      nombre: document.querySelector("#nombre").value,
+      marca: document.querySelector("#marca").value,
+      modelo: document.querySelector("#modelo").value,
+      precio: document.querySelector("#precio").value,
+      foto: document.querySelector("#imagen").value,
+      id_categoria: document.querySelector("#slc_categoria").value,
     };
+    console.log(nuevoProducto);
     try {
-      await axios.post(`http://localhost:3030/productos/`,nuevoProducto)
-        //limpiamos el formulario
-        formCrearPosteo.reset()
-        // recargue los posteos actualizado
-        fetchProductos()
+      await axios.post(`http://localhost:3030/productos/`, nuevoProducto);
+      //limpiamos el formulario
+      formCrearPosteo.reset();
+      // recargue los posteos actualizado
+      fetchProductos();
     } catch (error) {
-      console.error (`Error al postear: ${error}`)
+      console.error(`Error al postear: ${error}`);
     }
-  })
+  });
 
   fetchProductos();
+  fetchCategoria();
 });
